9c5f456359f23a35a3381ee238b852d0fa12a547
Saidnazar <dadajohn137@gmail.com>
2021-02-22 22:43:01 -0800

    collected images

---
+++ assignments/week6/main.js
@@ -0,0 +1,32 @@
+// How to use a function: functionName();
+console.log('Hello Saidnazar');
+
+console.log('Bye');
+
+var myFirstVariable;
+
+// The three most common Datatypes
+
+// Strings
+
+var myFirstVariable = 'My name is Saidnazar';
+console.log(myFirstVariable);
+
+var mySecondVaraible = 'My name is Sean';
+
+// Number
+10;
+9;
+100000;
+0;
+('10'); // Not a Number, it is a String instead
+
+// Booleans
+true;
+false;
+
+// Mathematical Operators
+10 + 10; //20
+5 - 10; //-5
+2 / 5; //.4
+10 * 5; //50
