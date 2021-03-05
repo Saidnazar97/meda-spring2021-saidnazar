2046b45373a79b2e9a60ae3d7c260672e9bb2ffa
Saidnazar <dadajohn137@gmail.com>
2021-03-05 09:09:27 -0800

    Budget Calculator

--- assignments/week6/main.js
+++ assignments/week6/main.js
@@ -30,3 +30,49 @@ false;
 5 - 10; //-5
 2 / 5; //.4
 10 * 5; //50
+
+// Conditional Statements. If statement needs to have a true value in order to run / execute the code in its code block.
+
+// Conditonal statement syntax
+//if (true or false value) { code to run based on condition in parenthesis. }
+
+var numberToTest = 0;
+
+if (numberToTest > 0) {
+  console.log('The number is positive.');
+} else {
+  console.log('The number is not positive');
+}
+
+if (numberToTest == 0) {
+  console.log('The number is zero');
+} else {
+  console.log('The number is not zero');
+}
+
+if (numberToTest < 0) {
+  console.log('The number is negative');
+} else {
+  console.log('The number is not negative');
+}
+var personAge;
+
+personAge = 40;
+
+if (personAge >= 21 && personAge <= 25) {
+  console.log('Congrats! you are of drinking age');
+} else if (personAge >= 26 && personAge <= 40) {
+  console.log('Do you want to go grab a beer');
+} else {
+  console.log('Have you tried video games');
+}
+
+// While
+
+var continueRunning = 0;
+
+while (continueRunning < 10) {
+  console.log('We ran the loop once!');
+
+  continueRunning = continueRunning + 1;
+}
